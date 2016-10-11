defe7033823fcb0926ac286772989fd4bbde473a
obi <obi_lipnik@yahoo.co.uk>
2016-10-11 22:34:15 +0200

    Skeleton addition

--- bot/views.py
+++ bot/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 
+from . api import bitstamp
 # Create your views here.
+
+@login_required
+def index(request):
+    context = {}
+    return render(request, 'bot/index.html', context)
+
